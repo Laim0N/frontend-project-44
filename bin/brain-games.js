81ac146cec90c90be467e6362c1534dc6180c988
Laim0N <Laim0n1995@gmail.com>
2023-04-30 18:13:51 +1000

    Added brainGcd game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 import evenGame from '../games/brain-even.js'
 import brainCalc from '../games/brain-calc.js';
+import brainGcd from '../games/brain-gcd.js';
 import { userGreeting } from '../src/cli.js';
 
 userGreeting();
 evenGame();
 brainCalc();
+brainGcd();
---
+++ games/brain-gcd.js
@@ -0,0 +1,32 @@
+import { randomNum, playGame } from "../src/index.js";
+const gcd = (a, b) => {
+  a = Math.abs(a);
+  b = Math.abs(b);
+  if (a === 0) {
+    return b;
+  } else if (b === 0) {
+    return a;
+  }
+  while (a !== b) {
+    if (a > b) {
+       a = a - b;
+    } else {
+      b = b - a;
+    }
+  }
+  return a;
+};
+
+const brainGcd = () => {
+  console.log('Find the greatest common divisor of given numbers.');
+  const questionsAndAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const num1 = randomNum();
+    const num2 = randomNum();
+    const question = `Question: ${num1} ${num2}`;
+    const correctAnswer = gcd(num1, num2);
+    questionsAndAnswers.push([question, correctAnswer.toString()])
+  }
+  playGame(questionsAndAnswers);
+};
+export default brainGcd;
