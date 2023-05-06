1bc9bd28928f07e0f4ae5ff7c962e0608ce1bbac
Laim0N <Laim0n1995@gmail.com>
2023-05-07 09:50:59 +1000

    I created the brain-prime game.

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -4,9 +4,11 @@ import brainCalc from './games/brain-calc.js';
 import brainGcd from './games/brain-gcd.js';
 import brainProgression from './games/brain-progression.js';
 import { userGreeting } from '../src/cli.js';
+import primeGame from './games/brain-prime.js';
 
 userGreeting();
 evenGame();
 brainCalc();
 brainGcd();
 brainProgression();
+primeGame();
---
+++ bin/games/brain-prime.js
@@ -0,0 +1,25 @@
+import { getRandomInt, playGame } from '../../src/index.js';
+
+const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
+  for (let i = 0; i <= Math.sqrt(number); i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+const primeGame = () => {
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const questionsAndAnswers = [];
+  for (let i = 0; i < 3; i += 1) {
+    const randomNum = getRandomInt(1, 1000);
+    const correctAnswer = isPrime(randomNum) ? 'yes' : 'no';
+    const question = `Question: ${randomNum}`;
+    questionsAndAnswers.push([question, correctAnswer]);
+  }
+  playGame(questionsAndAnswers);
+};
+export default primeGame;
