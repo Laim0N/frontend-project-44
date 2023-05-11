cd39e4d80a15660baa30dfc0406027fa8488f9bc
Laim0N <Laim0n1995@gmail.com>
2023-05-11 20:06:24 +1000

    Implemented the ability to run games individually

--- Makefile
+++ Makefile
@@ -4,6 +4,21 @@ install:
 brain-games:
 	node bin/brain-games.js
 
+brain-calc:
+	node bin/calc-game.js
+
+brain-even:
+	node bin/even-game.js 
+
+brain-gcd:
+	node bin/gcd-game.js
+
+brain-prime:
+	node bin/prime-game.js
+
+brain-progression:
+	node bin/progression-game.js
+
 publish:
 	npm publish --dry-run
 
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
+import { userGreeting } from '../src/cli.js';
 import evenGame from '../src/games/brain-even.js';
 import brainCalc from '../src/games/brain-calc.js';
 import brainGcd from '../src/games/brain-gcd.js';
 import brainProgression from '../src/games/brain-progression.js';
-import { userGreeting } from '../src/cli.js';
 import primeGame from '../src/games/brain-prime.js';
 
 userGreeting();
